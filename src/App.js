04b4d290ff25ba1ce2f2772b1cd0c8ccfc57a7ca
orgmatter <o.ogadimma@mufin.co.kr>
2024-08-28 16:52:53 +0100

    test assesment complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+import FormControls from './components/FormControls';
+
+function App() {
+  return (
+    <div className="app-cover">
+      <FormControls />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FormControls.js
@@ -0,0 +1,105 @@
+import React, {useState} from "react";
+
+function ControlList(props) {
+
+    const { controlObj, indexKey, isChecked } = props;
+
+    return (
+        <div className="content-cover-flex-row" key={indexKey}>
+            <div className="content-cover-flex-item">
+                <span className="text-sp">{controlObj.chkboxName}</span>
+            </div>
+            <div className="content-cover-flex-item">
+                <label className="chkbox-label" htmlFor={`single-chkbox-${indexKey}`}>
+                    <input 
+                        className="form-chkbox" 
+                        id={`single-chkbox-${indexKey}`} 
+                        type="checkbox" 
+                        value={controlObj.chkboxVal}
+                        checked={isChecked? isChecked : null}
+                    />
+                    <span className="chkbox-mark">
+                        <svg viewBox="0 0 24 24">
+                            <path d="M9 16.2l-3.5-3.5L4 14.3l5 5L20 8.3l-1.4-1.4z"/>
+                        </svg>
+                    </span>
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default function FormControls() {
+
+    const [isChecked, setIsChecked] = useState(false)
+
+    const controlListArr = [
+        {
+            chkboxName: "Page 1",
+            chkboxVal: "page 1"
+        },
+        {
+            chkboxName: "Page 2",
+            chkboxVal: "page 2"
+        },
+        {
+            chkboxName: "Page 3",
+            chkboxVal: "page 3"
+        },
+        {
+            chkboxName: "Page 4",
+            chkboxVal: "page 4"
+        }
+    ]
+
+    const handleAllCheckBoxChange = () => {
+        setIsChecked(prev => !prev)
+    }
+    
+    return (
+        <div className="form-control-component-flex">
+            <div className="form-control-component-item">
+                <div className="form-control-cover-flex">
+                    <div className="form-control-cover-item">
+                        <div className="header-cover-flex">
+                            <div className="header-cover-item">
+                                <span className="text-sp">All pages</span>
+                            </div>
+                            <div className="header-cover-item">
+                                <label className="chkbox-label" htmlFor="all-chkbox">
+                                    <input 
+                                        className="form-chkbox" 
+                                        id="all-chkbox" 
+                                        type="checkbox"
+                                        onChange={handleAllCheckBoxChange}
+                                    />
+                                    <span className="chkbox-mark">
+                                        <svg viewBox="0 0 24 24">
+                                            <path d="M9 16.2l-3.5-3.5L4 14.3l5 5L20 8.3l-1.4-1.4z"/>
+                                        </svg>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="content-cover-flex-col">
+                            {
+                                controlListArr.map((controlObj, index) => {
+                                    return (<ControlList 
+                                                indexKey={index} 
+                                                controlObj={controlObj}
+                                                isChecked={isChecked}
+                                            />)
+                                })
+                            }
+                        </div>
+                        <div className="footer-cover-flex">
+                            <div className="footer-cover-item">
+                                <button className="form-btn" type="button">Done</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
